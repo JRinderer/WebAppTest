986446f296a9adc55c56170c4142b448dedb299f
John Rinderer <31448533+JRinderer@users.noreply.github.com>
2024-10-21 18:52:58 -0500

    Update dvwaPage.js

--- dvwa/js/dvwaPage.js
+++ dvwa/js/dvwaPage.js
@@ -3,6 +3,7 @@
 function popUp(URL) {
 	day = new Date();
 	id = day.getTime();
+	#test
 	window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=800,height=300,left=540,top=250');
 	//eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=800,height=300,left=540,top=250');");
 }
